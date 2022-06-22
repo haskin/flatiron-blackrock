ba8174ff44ab5acacd569bbc7f9c422a2a0d7dc8
Adrian Khaskin <haskin.dev@gmail.com>
2022-06-22 15:06:25 -0400

    Loop Lab added

---
+++ src/main/java/dev/haskin/_01/LoopLab.java
@@ -0,0 +1,32 @@
+package dev.haskin._01;
+
+public class LoopLab {
+    public static void main(String[] args) {
+        // simulate the passing of time
+        int startingYear = 2000;
+        int targetYear = 2011;
+        int currentYear = startingYear + 1;
+        while (currentYear < targetYear) {
+            if (currentYear - startingYear == 1) {
+                System.out.println((currentYear - startingYear) + " year have passed");
+                currentYear++;
+                continue;
+            }
+
+            System.out.println((currentYear - startingYear) + " year(s) have passed");
+            currentYear++;
+        }
+        // extension(startingYear, targetYear);
+    }
+
+    public static void extension(int startingYear, int targetYear) {
+        for (int currentYear = startingYear + 1; currentYear < targetYear; currentYear++) {
+            if (currentYear - startingYear == 1) {
+                System.out.println((currentYear - startingYear) + " year have passed");
+                currentYear++;
+                continue;
+            }
+            System.out.println((currentYear - startingYear) + " year(s) have passed");
+        }
+    }
+}
